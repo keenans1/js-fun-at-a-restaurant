d45440533bfef24b97c7654db64aa8ef95a15cb7
Keenan Southall <keenan.southall@yahoo.com>
2022-10-14 14:19:15 -0600

    Add functions for tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+}
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+
+  return menuItem;
+}
+
+function addIngredients(ingredient, allIngredients) {
+  if (!allIngredients.includes(ingredient)) {
+    allIngredients.push(ingredient);
+  }
+
+  return allIngredients;
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  return price * .9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+
+  return recipe;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
