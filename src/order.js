45cb74067955b0693f4e9d6578e8e15b0b81ca71
Keenan Southall <keenan.southall@yahoo.com>
2022-10-15 21:02:21 -0600

    Add functions for tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,58 @@
+function takeOrder(order, deliveryOrders) {
+
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(order);
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+
+  for (var i = 0; i < deliveryOrders.length; i++) {
+
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+
+      deliveryOrders.splice(i, 1);
+    }
+  }
+
+  return deliveryOrders;
+}
+
+function listItems(deliveryOrders) {
+
+  var orderNames = '';
+
+  for (var i = 0; i < deliveryOrders.length; i++) {
+
+    if (i === deliveryOrders.length - 1) {
+      orderNames += deliveryOrders[i].item;
+    } else {
+      orderNames += deliveryOrders[i].item + ', ';
+    }
+  }
+
+  return orderNames;
+}
+
+function searchOrder(deliveryOrders, item) {
+
+  for (var i = 0; i < deliveryOrders.length; i++) {
+
+    if (deliveryOrders[i].item === item) {
+      return true;
+    }
+  }
+
+
+
+
+  return false;
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
